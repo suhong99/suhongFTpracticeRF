dbf3e09eb6620a1eeeb49aeb46ad54d7a446c886
suhong99 <topic828@naver.com>
2023-08-21 20:03:16 +0900

    푸쉬

--- chapter7-disney/src/App.js
+++ chapter7-disney/src/App.js
@@ -1,51 +1,32 @@
-import './App.css';
-import Banner from './components/Banner';
-import Category from './components/Category';
-
-import Nav from './components/Nav';
-import styled from 'styled-components';
-import Row from './components/Row';
-import requests from './api/requests';
-
-function App() {
+import "./App.css";
+import React from "react";
+import { Outlet, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import LoginPage from "./pages/LoginPage";
+import MainPage from "./pages/MainPage";
+import DatailPage from "./pages/DetailPage";
+import SearchPage from "./pages/SearchPage";
+const Layout = () => {
   return (
-    <Container>
+    <div>
       <Nav />
-      <Banner />
-      <Category />
-      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
-      <Row
-        title="Action Movies"
-        id="AM"
-        fetchUrl={requests.fetchActionMovies}
-      />
-      <Row
-        title="Comedy Movies"
-        id="CM"
-        fetchUrl={requests.fetchComedyMovies}
-      />
-    </Container>
+      <Outlet />
+    </div>
   );
-}
+};
+const App = () => {
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<LoginPage />} />
+          <Route path="main" element={<MainPage />} />
+          <Route path=":movieId" element={<DatailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
-
-const Container = styled.main`
-  position: relative;
-  min-height: calc(100vh - 250px);
-  overflow-x: hidden;
-  display: block;
-  top: 72px;
-  padding: 0 calc(3.5vw + 5px);
-
-  &:after {
-    background: url('/images/home-background.png') center center / cover
-      no-repeat fixed;
-    content: '';
-    position: absolute;
-    inset: 0px;
-    opacity: 1;
-    z-index: -1;
-  }
-`;
--- chapter7-disney/src/components/Nav.js
+++ chapter7-disney/src/components/Nav.js
@@ -1,12 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 const Nav = () => {
+  const { pathname } = useLocation();
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
 
+  const navigate = useNavigate();
+  // console.log("pathname " + pathname);
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
   const handleScroll = () => {
@@ -17,20 +22,96 @@ const Nav = () => {
     }
   };
 
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
   return (
     <NavWrapper $show={show}>
       <Logo>
-        <img
-          alt="Disney Plus Logo"
-          src="/images/logo.svg"
-          onClick={() => (window.location.href = '/')}
-        />
+        <img alt="Disney Plus Logo" src="/images/logo.svg" onClick={() => (window.location.href = "/")} />
       </Logo>
+      {pathname === "/" ? (
+        <Login>Login</Login>
+      ) : (
+        <Input
+          value={searchValue}
+          onChange={handleChange}
+          className="nav__input"
+          type="text"
+          placeholder="검색해주세요."
+        />
+      )}
     </NavWrapper>
   );
 };
 
 export default Nav;
+
+// const DropDown = styled.div`
+//   position: absolute;
+//   top: 48px;
+//   right: 0px;
+//   background: rgb(19, 19, 19)
+//   border: 1px solid rgba(151, 151, 151, 0.34);
+//   border-radius:  4px;
+//   box-shadow: rgb(0 0 0 /50%) 0px 0px 18px 0px;
+//   padding: 10px;
+//   font-size: 14px;
+//   letter-spacing: 3px;
+//   width: 100%;
+//   opacity: 0;
+// `;
+
+// const SignOut = styled.div`
+//   position: relative;
+//   height: 48px;
+//   width: 48px;
+//   display: flex;
+//   cursor: pointer;
+//   align-items: center;
+//   justify-content: center;
+
+//   &:hover {
+//     ${DropDown} {
+//       opacity: 1;
+//       transition-duration: 1s;
+//     }
+//   }
+// `;
+
+// const UserImg = styled.img`
+//   border-radius: 50%;
+//   width: 100%;
+//   height: 100%;
+// `;
+
+const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: gray;
+    border-color: transparent;
+  }
+`;
+
+const Input = styled.input`
+  position: fixed;
+  left: 50%;
+  transform: translate(-50%, 0);
+  background-color: rgba(0, 0, 0, 0.582);
+  border-radius: 5px;
+  color: white;
+  padding: 5px;
+  border: none;
+`;
 
 const NavWrapper = styled.nav`
   position: fixed;
@@ -38,13 +119,13 @@ const NavWrapper = styled.nav`
   left: 0;
   right: 0;
   height: 70px;
-  background-color: ${(props) => (props.$show ? '#090b13' : 'transparent')};
+  background-color: ${(props) => (props.show ? "#090b13" : "transparent")};
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0 36px;
   letter-spacing: 16px;
-  z-index: 3; // 네비는 위로 올라가야 하니깐
+  z-index: 3;
 `;
 
 const Logo = styled.a`
@@ -53,7 +134,7 @@ const Logo = styled.a`
   margin-top: 4px;
   max-height: 70px;
   font-size: 0;
-  display: inline-block;
+  diplay: inline-block;
 
   img {
     display: block;
