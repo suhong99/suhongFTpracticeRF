4a1e30fc3a9dc1a94dd8210188564a1a5dac9635
SUHONG\topic <topic828@Naver.com>
2023-08-31 02:36:40 +0900

    도커실습

--- chapter7-disney/src/pages/MainPage.js
+++ chapter7-disney/src/pages/MainPage.js
@@ -1,10 +1,10 @@
-import React from "react";
-import Nav from "../components/Nav";
-import Banner from "../components/Banner";
-import Row from "../components/Row";
-import requests from "../api/requests";
-import Category from "../components/Category";
-import { styled } from "styled-components";
+import React from 'react';
+import Nav from '../components/Nav';
+import Banner from '../components/Banner';
+import Row from '../components/Row';
+import requests from '../api/requests';
+import Category from '../components/Category';
+import { styled } from 'styled-components';
 
 const MainPage = () => {
   return (
@@ -15,8 +15,16 @@ const MainPage = () => {
 
       <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
     </Container>
   );
 };
@@ -32,8 +40,9 @@ const Container = styled.main`
   padding: 0 calc(3.5vw + 5px);
 
   &:after {
-    background: url("/images/home-background.png") center center / cover no-repeat fixed;
-    content: "";
+    background: url('/images/home-background.png') center center / cover
+      no-repeat fixed;
+    content: '';
     position: absolute;
     inset: 0px;
     opacity: 1;
